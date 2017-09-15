d8b45773d742c928740653b949c7e6cd423907ff
zhugy <zhugy781@163.com>
2017-09-15 09:53:51 +0800

    add

--- ThePromise/BaseThePromise/Controller/search/CoreTextViewController.swift
+++ ThePromise/BaseThePromise/Controller/search/CoreTextViewController.swift
@@ -13,6 +13,8 @@ class CoreTextViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
+        
         // Do any additional setup after loading the view.
     }
 
